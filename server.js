1828d612a2f4c7ae0c1691dfefff1a2b7d68607b
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-04-23 20:36:49 +0530

    CHG | Initial COmmit-Jobify Backend

---
+++ server.js
@@ -0,0 +1,12 @@
+import Express from "express";
+const app = Express()
+
+app.get('/', (req, res) => {
+    res.send('Welcome!!')
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is Running on Port ${port}..`)
+})
