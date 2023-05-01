3fe89835a3bc23da572a8bbe0861652371c51085
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-05-01 12:03:53 +0530

    CHG | Error Handler-Refactor ERRORS

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,6 @@
 import User from '../models/UserModel.js';
 import { StatusCodes } from 'http-status-codes';
-
-class CustomAPIError extends Error {
-    constructor(message) {
-        super(message)
-    }
-}
-class BadRequestError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = StatusCodes.BAD_REQUEST
-    }
-}
-
-class NotFoundError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = StatusCodes.NOT_FOUND
-    }
-}
+import { BadRequestError } from '../error/index.js';
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
