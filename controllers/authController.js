118c52d9e8f4445063b2ee3ec62823ebf7572656
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-05-01 12:39:23 +0530

    CHG | Error Handler-Check for duplicate email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,19 @@
 import User from '../models/UserModel.js';
-import { StatusCodes } from 'http-status-codes';
+import { CONFLICT, StatusCodes } from 'http-status-codes';
 import { BadRequestError } from '../error/index.js';
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
-
+    
     if (!name || !email || !password) {
         throw new BadRequestError('Please provide all values')
     }
-    const user = await User.create(req.body)
+
+    const userAlreadyExists =  await User.findOne({email})
+    if(userAlreadyExists) {
+        throw new BadRequestError('Email already in use')
+    }
+    const user = await User.create(name, email, password)
     res.status(StatusCodes.CREATED).json({ user})   
 }
 
