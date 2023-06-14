041d362deadff36d5adaec29416efe4769980a1a
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-06-14 21:30:38 +0530

    CHG | Bearer Token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,8 @@
-const auth = (resq, res, next) => {
-    console.log('authenticate user');
+const auth = (req, res, next) => {
+    const headers = req.headers
+    const authHeaders = req.headers.authorization
+    console.log(headers);
+    console.log(authHeaders);
     next(); 
 }
 export default auth;
