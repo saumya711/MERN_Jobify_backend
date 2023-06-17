e4d62132ae87cfb4964403c3e776e2473f4a18f7
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-06-17 11:50:11 +0530

    CHG | Update Controller function

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,8 +47,22 @@ const login = async (req, res) => {
 }
 
 const updateUser = async (req, res) => {
-    console.log(req.user)
-    res.send('Update User Successfully');
+    const { email, name, lastName, location } = req.body;
+    if ( !email || !name || !lastName || !location) {
+        throw new BadRequestError('Please Provide all values');
+    }
+
+    const user = await User.findOne({ _id: req.user.userId});
+
+    user.email = email
+    user.name = name
+    user.lastName = lastName
+    user.location = location
+
+    await user.save()
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 
 export {
